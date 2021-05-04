1ff8d9f6db208bd0d82d47363d03a263baa4659b
Al_chemick <alex2015mig@gmail.com>
2021-05-05 01:37:11 +0300

    feat: add server

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("start");
-});
+const PORT = config.get("port") || 5000;
+const MONGO_URI = config.get("mongoUri");
+
+async function start() {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+}
+
+start();
