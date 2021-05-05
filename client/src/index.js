a8bbf1d7fbb8ca82129a386608129fb1c7c59a94
Al_chemick <alex2015mig@gmail.com>
2021-05-05 11:12:58 +0300

    feat: add client

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./main.css";
+import App from "./App";
+
+const app = document.getElementById("app");
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  app
+);
