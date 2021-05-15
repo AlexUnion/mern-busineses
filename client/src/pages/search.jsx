6d3a5c130841eaf4838c9aa50c063aed1b1196ec
Al_chemick <alex2015mig@gmail.com>
2021-05-15 21:53:15 +0300

    feat: add form to search page

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import "./search.css";
+import List from "../components/list/list.component";
 
 function Search() {
   return (
     <>
-      <h2 className="text-center">Пошук</h2>
+      <h2 className="text-center">Пошук бізнесу</h2>
       <form action="" method="POST">
-        <label>
-          <p>
-            Введіть назву міста<span className="required">*</span>
-          </p>
-          <input name="city" id="city" required placeholder="Місто" />
-        </label>
-        <select size="1" name="type" id="type">
-          <option value="none" selected>
-            Виберіть тип
-          </option>
-          <option value="cafe">Кафе</option>
-        </select>
+        <div className="form-item">
+          <label className="label">
+            <p>
+              Введіть назву міста<span className="required">*</span>
+            </p>
+            <input
+              name="city"
+              id="city"
+              required
+              placeholder="Місто"
+              className="input"
+            />
+          </label>
+        </div>
+        <div className="form-item">
+          <p>Виберіть тип бізнесу</p>
+          <select size="1" name="type" id="type" className="input">
+            <option value="none" selected disabled>
+              Тип
+            </option>
+            <option value="cafe">Кафе</option>
+          </select>
+        </div>
+        <div className="form-submit">
+          <button type="submit" className="btn">
+            Пошук
+          </button>
+        </div>
       </form>
     </>
   );
