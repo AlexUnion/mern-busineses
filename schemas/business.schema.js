20429f8ea2fc981c0d485f5cb925ac28669e9a1e
Al_chemick <alex2015mig@gmail.com>
2021-05-18 22:57:22 +0300

    feat: add business schema

---
+++ schemas/business.schema.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const businessSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  shortDescription: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  img: {
+    type: String,
+  },
+});
+
+module.exports = model("Business", businessSchema);
