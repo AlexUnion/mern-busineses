be81f88e02b1bac943d7e71b9901ddeb83a659b6
Al_chemick <alex2015mig@gmail.com>
2021-05-22 22:01:46 +0300

    feat: add correct router to main

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -3,31 +3,41 @@ const Business = require("../schemas/business.schema");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  try {
-    Business.find(async (err, doc) => {
-      if (err) {
-        console.log(err);
-      }
-      await res.json(doc);
-    });
-  } catch (e) {
-    console.log(e);
-  }
+    console.log(req.headers);
+    try {
+        Business.find(async (err, doc) => {
+            if (err) {
+                console.log(err);
+                res.status = 500;
+                res.send({
+                    message: 'Something went wrong',
+                    error: err
+                });
+            }
+            res.status = 200;
+            res.append('Access-Control-Allow-Origin', '*');
+            res.json(doc);
+        });
+    } catch (e) {
+        console.log(e);
+        res.status = 500;
+        res.send({ message: 'Something went wrong' });
+    }
 });
 
 router.post("/", async (req, res) => {
-  const business = new Business({
-    title: "Mala Opera Spot",
-    city: "Вінниця",
-    shortDescription:
-      "В кофейне Mala Opera Spot зал выполнен в стиле лофт, спокойных и ненавязчивых тонах. Мебель предельно простая и удобная, главным образом изготовлена из качественной древесины.",
-    description:
-      "В кофейне Mala Opera Spot зал выполнен в стиле лофт, спокойных и ненавязчивых тонах. Мебель предельно простая и удобная, главным образом изготовлена из качественной древесины.",
-    img:
-      "https://assets.turbologo.ru/blog/ru/2020/02/18162724/a5a4b6991268b801e30d82bbc757c672-958x575.jpg",
-  });
-  business.save();
-  res.json({ message: "saved" });
+    const business = new Business({
+        title: "Mala Opera Spot",
+        city: "Вінниця",
+        shortDescription:
+            "В кофейне Mala Opera Spot зал выполнен в стиле лофт, спокойных и ненавязчивых тонах. Мебель предельно простая и удобная, главным образом изготовлена из качественной древесины.",
+        description:
+            "В кофейне Mala Opera Spot зал выполнен в стиле лофт, спокойных и ненавязчивых тонах. Мебель предельно простая и удобная, главным образом изготовлена из качественной древесины.",
+        img:
+            "https://assets.turbologo.ru/blog/ru/2020/02/18162724/a5a4b6991268b801e30d82bbc757c672-958x575.jpg",
+    });
+    business.save();
+    res.json({ message: "saved" });
 });
 
 module.exports = router;
