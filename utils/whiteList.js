f22df197af60a624df083e9ed541cb9eb3ef93be
Al_chemick <alex2015mig@gmail.com>
2021-05-30 19:26:55 +0300

    fix: add access-controll headers

--- utils/whiteList.js
+++ utils/whiteList.js
@@ -8,11 +8,13 @@ const useWhiteList = (req, res, next) => {
     const isAllow = whiteList.find((value => value === origin));
     if (isAllow) {
         res.append("Access-Control-Allow-Origin", origin);
+        res.append("Access-Control-Allow-Methods", "GET, POST");
+        res.append("Access-Control-Allow-Headers", "Content-Type");
         res.append("Vary", "Origin");
         next();
     } else {
         res.send();
     }
-}
+};
 
 module.exports = useWhiteList;
