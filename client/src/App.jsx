3d160f70876aef372c309ac314ae56d9f8d6582c
Al_chemick <alex2015mig@gmail.com>
2021-05-30 18:44:31 +0300

    feat: add "Add" & "Search" pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,36 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.component";
 import List from "./components/list/list.component";
 import MainLayout from "./layout/mainLayout";
-import Search from "./pages/search";
+import Search from "./pages/search/search";
+import Add from "./pages/add/add";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <MainLayout>
-        <Switch>
-          <Route path="/about">
-            <h2>About</h2>
-          </Route>
-          <Route path="/add">
-            <h2>add</h2>
-          </Route>
-          <Route exact path="/search" component={Search} />
-          <Route path="/" component={List} />
-        </Switch>
-      </MainLayout>
-      <footer className="text-center">
-        All rights reserved © Teterev Vitaliy 2021
-      </footer>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header/>
+            <MainLayout>
+                <Switch>
+                    <Route path="/about">
+                        <h2>About</h2>
+                    </Route>
+                    <Route path="/add" component={Add}/>
+                    <Route exact path="/search"
+                           component={Search}/>
+                    <Route path="/" component={List}/>
+                </Switch>
+            </MainLayout>
+            <footer className="text-center">
+                All rights reserved © Teterev Vitaliy 2021
+            </footer>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- client/src/pages/search/search.jsx
+++ client/src/pages/search/search.jsx
@@ -1,31 +1,23 @@
 import React from "react";
 import Label from "../../components/label/label.component";
+import Selector from "../../components/selector/selector";
 
 function Search() {
     return (
         <div className='text-center'>
             <h2>Пошук бізнесу</h2>
-            <form action="" method="POST" className='text-left'>
+            <form action="" method="POST"
+                  className='text-left'>
                 <Label className="mt-6"
                        title="Введіть назву міста"
                        required
                        name="city"
                        id="city"
                        placeholder="Місто"/>
-                <div className='mt-6'>
-                    <p>Виберіть тип бізнесу</p>
-                    <select
-                        size="1"
-                        name="type"
-                        id="type"
-                        className="input"
-                        defaultValue="none"
-                    >
-                        <option value="cafe">Кафе</option>
-                        <option value="cafe">Міні-маркет
-                        </option>
-                    </select>
-                </div>
+                <Selector className="mt-6"
+                          title="Виберіть тип бізнесу"
+                          name="type"
+                          id="type"/>
                 <div className="mt-6 text-right">
                     <button type="submit" className="btn">
                         Пошук
