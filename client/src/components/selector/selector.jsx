ad39e075c510552704f1def9e2c1bf7769e16be1
Al_chemick <alex2015mig@gmail.com>
2021-05-30 18:02:05 +0300

    fix: add input property to "selector" component

--- client/src/components/selector/selector.jsx
+++ client/src/components/selector/selector.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { types } from "../../data/business.type";
 
-const Selector = ({ title }) => (
-    <div>
+const Selector = ({ title, className, input }) => (
+    <div className={className}>
         <p>{title}</p>
         <select
+            {...input}
             size="1"
             className="input"
             defaultValue="none">
-            {types.map(({ value, label }) => (
-                <option value={value}>{label}</option>
+            {types.map(({ value, label }, index) => (
+                <option key={index}
+                        value={value}>
+                    {label}
+                </option>
             ))}
         </select>
     </div>
